5ef5603eee9309c308ed8ea275e3eaa9ba766724
fidja <livioam@gmail.com>
2015-09-16 22:55:50 -0300

    First script to teste

---
+++ script1.lua
@@ -0,0 +1,158 @@
+randTry = random(15,35)
+
+say("c")
+minWaitFish = 1890
+maxWaitFish = 2750
+
+if itemcount("worm")>0 then
+
+	for i=1, randTry, 1 do
+		
+		if itemcount("worm")>0 then
+	
+			randSQMFish = random(1,26)
+
+			if randSQMFish == 1 then
+
+				useitemxyz("fishing rod",sqm1x,sqm1y,posz)
+				wait(minWaitFish,maxWaitFish)
+			
+			elseif randSQMFish == 2 then
+
+				useitemxyz("fishing rod",sqm2x,sqm2y,posz)
+				wait(minWaitFish,maxWaitFish)
+			
+			elseif randSQMFish == 3 then
+
+				useitemxyz("fishing rod",sqm3x,sqm3y,posz)
+				wait(minWaitFish,maxWaitFish)
+			
+			elseif randSQMFish == 4 then
+
+				useitemxyz("fishing rod",sqm4x,sqm4y,posz)
+				wait(minWaitFish,maxWaitFish)
+			
+			elseif randSQMFish == 5 then
+
+				useitemxyz("fishing rod",sqm5x,sqm5y,posz)
+				wait(minWaitFish,maxWaitFish)
+			
+			elseif randSQMFish == 6 then
+
+				useitemxyz("fishing rod",sqm6x,sqm6y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 7 then
+
+				useitemxyz("fishing rod",sqm7x,sqm7y,posz)
+				wait(minWaitFish,maxWaitFish)
+
+			elseif randSQMFish == 8 then
+
+				useitemxyz("fishing rod",sqm8x,sqm8y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 9 then
+
+				useitemxyz("fishing rod",sqm9x,sqm9y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 10 then
+
+				useitemxyz("fishing rod",sqm10x,sqm10y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 11 then
+
+				useitemxyz("fishing rod",sqm11x,sqm11y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 12 then
+
+				useitemxyz("fishing rod",sqm12x,sqm12y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 13 then
+
+				useitemxyz("fishing rod",sqm13x,sqm13y,posz)
+				wait(minWaitFish,maxWaitFish)
+
+			elseif randSQMFish == 14 then
+
+				useitemxyz("fishing rod",sqm14x,sqm14y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 15 then
+
+				useitemxyz("fishing rod",sqm15x,sqm15y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 16 then
+
+				useitemxyz("fishing rod",sqm16x,sqm16y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 17 then
+
+				useitemxyz("fishing rod",sqm17x,sqm17y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 18 then
+
+				useitemxyz("fishing rod",sqm18x,sqm18y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 19 then
+
+				useitemxyz("fishing rod",sqm19x,sqm19y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 20 then
+
+				useitemxyz("fishing rod",sqm20x,sqm20y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 21 then
+
+				useitemxyz("fishing rod",sqm21x,sqm21y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 22 then
+
+				useitemxyz("fishing rod",sqm22x,sqm22y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 23 then
+
+				useitemxyz("fishing rod",sqm23x,sqm23y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			elseif randSQMFish == 24 then
+
+				useitemxyz("fishing rod",sqm24x,sqm24y,posz)
+				wait(minWaitFish,maxWaitFish)
+
+			elseif randSQMFish == 25 then
+
+				useitemxyz("fishing rod",sqm25x,sqm25y,posz)
+				wait(minWaitFish,maxWaitFish)
+
+			elseif randSQMFish == 26 then
+
+				useitemxyz("fishing rod",sqm26x,sqm26y,posz)
+				wait(minWaitFish,maxWaitFish)
+		
+			else
+
+				wait(350,780)
+
+			end 
+
+		end
+		say("b")
+	end
+
+end
+
+
+wait(120000,140000)
